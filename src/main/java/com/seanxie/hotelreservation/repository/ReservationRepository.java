8bdd57a25bb0f51c8b4006529fc855b80c7c45b9
seanx07 <xie07@email.franklin.edu>
2018-09-12 15:51:19 -0500

    Revert "Hotel Reservation Full Stack App"
    This reverts commit 026b851c2d2cabe7207fd7f8370339525fe051eb.

--- src/main/java/com/seanxie/hotelreservation/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seanxie.hotelreservation.repository;
-
-import com.seanxie.hotelreservation.entity.ReservationEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReservationRepository extends CrudRepository<ReservationEntity, Long> {
-}
